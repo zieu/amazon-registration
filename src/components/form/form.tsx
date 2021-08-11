3a688955171f6dabc1cba7a1770cb062084462b9
Ziyodulla Muydinjanov <zieu2000@gmail.com>
2021-08-11 10:06:39 +0500

    form & input styles

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -1,4 +1,5 @@
 import { AlertBox } from "components";
+import Input from "components/input";
 import classes from "./form.module.scss";
 
 const probs = ["prob1", "prob2"];
@@ -7,7 +8,12 @@ const Form = () => {
 	return (
 		<div className={classes.container}>
 			<AlertBox problemsList={probs} />
-			<form className={classes.form}></form>
+			<form className={classes.form}>
+				<h1 className={classes.title}>Create account</h1>
+				<Input label="Your name" type="text" />
+				<Input label="Email" type="email" />
+				<Input label="Password" type="password" />
+			</form>
 		</div>
 	);
 };
--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -10,12 +10,12 @@ export type Props = HTMLProps<HTMLInputElement> & {
 
 const Input: FC<Props> = ({ label, touched = false, onChange, id, ...inputProps }) => {
 	return (
-		<>
+		<div className={classes.container}>
 			<label className={classes.label} htmlFor={id}>
 				{label}
 			</label>
 			<input className={classes.input} type="text" id={id} {...inputProps} />
-		</>
+		</div>
 	);
 };
 
